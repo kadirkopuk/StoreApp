77ad9649282dd3ea675c5d596d05b39f4452c609
kadirkopuk <kadirkopuk.ybs@gmail.com>
2023-07-24 16:54:21 +0300

    Connection String

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,7 +3,7 @@ namespace StoreApp.Models
     public class Product
     {
         public int ProductId {get; set; }
-
         public String? ProductName {get; set;} = String.Empty;
+        public Decimal Price {get; set;}
     }
 }
