9af84b2005bbbf0de05e38621633f4409c67a51d
kadirkopuk <kadirkopuk.ybs@gmail.com>
2023-08-03 11:29:36 +0300

    create Repositories

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-using StoreApp.Models;
+using Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<RepositoryContext>(options => {
-    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
+    options => options.MigrationsAssembly("StoreApp"));
 });
 
 var app = builder.Build();
