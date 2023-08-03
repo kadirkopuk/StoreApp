9f9a81b558ab6fcbb765584e74b1ac7b785e00ff
kadirkopuk <kadirkopuk.ybs@gmail.com>
2023-08-03 14:22:00 +0300

    create Repository Manager

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Repositories;
+using Repositories.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,10 @@ builder.Services.AddDbContext<RepositoryContext>(options => {
     options => options.MigrationsAssembly("StoreApp"));
 });
 
-var app = builder.Build();
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build(); 
 
 
 app.UseStaticFiles();
